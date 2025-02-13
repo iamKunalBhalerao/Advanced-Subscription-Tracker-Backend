5438a41c8e4d3f7da34176d648d3c6db6e1436f9
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-14 00:59:06 +0530

    3: created first node server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("server is stared");
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(3000, () => {
+  console.log("server is on PORT:3000");
+});
