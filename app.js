cc87752210d8bcda02f3610582ba1800fdd9d219
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-14 09:03:47 +0530

    5: Configure all the PORT data from config

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
-const app = express();
+import { PORT } from "./config/env.js";
 
+const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(3000, () => {
-  console.log("Subscription Tracker API is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
 });
 
 export default app;
- 
