c8c35f8faf851b411de1c8a35475262f6ba2b2b6
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-14 08:56:09 +0530

    4: Adding dotenv package for port and urls for production and development livel

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("server is on PORT:3000");
+  console.log("Subscription Tracker API is running on http://localhost:3000");
 });
+
+export default app;
+ 
