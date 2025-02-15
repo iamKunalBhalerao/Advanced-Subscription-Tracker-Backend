289df7e2612d3b46e030cad7c97d4565f5b58a93
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-15 16:16:28 +0530

    10: Adding some default express js middleware to handle errors in app

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import { PORT } from "./config/env.js";
+
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auuth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+
 import connectToDatabase from "./database/db.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
+
+app.get("/", (req, res) => res.send("Welcome to Subscription Tracker API"));
 
 app.listen(PORT, async () => {
   console.log(
