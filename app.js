fe8a52a28163a0184c8b74ec8d891230cf1c463d
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-15 16:09:33 +0530

    9: Adding Advanced Error Handling using errorMiddleware and integrated in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auuth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/db.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(
