e0705aba89f53bd9157f3cbaa1267a25929ffc83
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-17 19:50:42 +0530

    16: Added ARCJET rate limiting logic and request handler like functionality so any bot cant harm our app

---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,28 @@
+import aj from "../config/arcjet.js";
+
+const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({
+          message: "TOO Many Requests !!!",
+        });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({
+          message: "No BOT's Allowed !!!",
+        });
+      } else {
+        res.status(403).json({
+          message: "Access Denied !!!",
+        });
+      }
+    }
+
+    next();
+  } catch (error) {
+    console.log(`Arcjet middleware error:`, error);
+  }
+};
+export default arcjetMiddleware;
